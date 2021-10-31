1420baf3faf8dcaf50021bc525556ffefce3a45c
kevin <kevinchannon@gmail.com>
2021-10-31 07:15:27 +0000

    Adding spdlog logging to project

--- RegisterHelper/main.cpp
+++ RegisterHelper/main.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <bitset>
 
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/stdout_color_sinks.h"
+
 ///////////////////////////////////////////////////////////////////////////////
 
 using MyRange = RegisterBaseAddressRange<uint64_t, 0x00000000, 0x00001000>;
@@ -21,21 +24,25 @@ using TestRange3 = bitmask::SingleBit<MyRegister, 33>;
 
 int main()
 {
+    auto console    = spdlog::stdout_color_mt("console");
+    auto err_logger = spdlog::stderr_color_mt("stderr");
+    spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
+
     auto r1 = RegisterValue<MyRegister>(0u);
 
     r1.set<Status>(true);
     r1.set<TestRange1>(29);
-    r1.set<TestRange2>(66458843794);
+    r1.set<TestRange2>(423423); // 66458843794);
     r1.set<TestRange3>(true);
 
     auto x = std::bitset<bitmask::WORD_SIZE * sizeof(MyRegister::Value_t)>(r1.raw());
 
-    std::cout << "Raw value = " << x << std::endl;
+    spdlog::info("Raw value = {}", x.to_ullong());
 
-    std::cout << "Status = " << std::boolalpha << r1.get<Status>() << std::endl;
-    std::cout << "Range 1 = " << r1.get<TestRange1>() << std::endl;
-    std::cout << "Range 2 = " << r1.get<TestRange2>() << std::endl;
-    std::cout << "Range 3 = " << std::boolalpha << r1.get<TestRange3>() << std::endl;
+    spdlog::info("Status = {}", r1.get<Status>());
+    spdlog::info("Range 1 = {}", r1.get<TestRange1>());
+    spdlog::info("Range 2 = {}", r1.get<TestRange2>());
+    spdlog::info("Range 3 = {}", r1.get<TestRange3>());
 
     return 0;
 }
